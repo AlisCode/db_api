f72754e350ec03eb3a24009d77d82ef03a6baa56
Olivier Pinon <oliv.pinon@gmail.com>
2019-10-18 18:02:15 +0200

    Migrate rocket example to use endpoints and services

--- examples/rocket.rs
+++ examples/rocket.rs
@@ -1,8 +1,8 @@
-use db_api::mounter::rocket_other::RocketHandlerOther;
-use db_api::retriever::rocket::{RocketRetriever, RocketRetrieverError};
-use db_api::retriever::Retriever;
+use db_api::endpoint::Endpoint;
+use db_api::mounter::rocket::RocketMounter;
+use db_api::mounter::Mounter;
 use db_api::retriever::{BodyRetriever, DeserializeRetriever, UniqueStateRetriever};
-use rocket::http::Method;
+use http::Method;
 use rocket::response::Responder;
 use rocket::{Rocket, Route};
 use serde::{Deserialize, Serialize};
@@ -57,9 +57,16 @@ impl Counter {
         *val = *val + 1;
         *val
     }
+
+    pub fn add_val(&self, v: u32) -> u32 {
+        let mut val = self.val.lock().unwrap();
+        *val = *val + v;
+        *val
+    }
 }
 
-fn handle_count(counter: &Counter) -> String {
+// State retriever
+fn handle_count(counter: Arc<Counter>) -> String {
     let new_val = counter.count();
     format!("This page has been visited {} times", new_val)
 }
@@ -68,12 +75,18 @@ fn retrievers_count() -> UniqueStateRetriever<Counter> {
     UniqueStateRetriever::new()
 }
 
-fn test_ir<'a, 'r>(
-    ir: impl Retriever<RocketRetriever<'a, 'r>, RocketRetrieverError, Output = &'r Counter> + 'static,
-) {
+// Multiple retrievers
+fn handle_count_deser((counter, a): (Arc<Counter>, A)) -> String {
+    let new_val = counter.add_val(a.val);
+    format!("This common count is {}", new_val)
+}
+
+fn retrievers_count_deser() -> (UniqueStateRetriever<Counter>, DeserializeRetriever<A>) {
+    (UniqueStateRetriever::new(), DeserializeRetriever::new())
 }
 
 fn main() {
+    /*
     let my_handler = RocketHandlerOther::new(Method::Get, "/test".into(), handle, retrievers);
     let my_handler_route = Route::new(Method::Get, "/test", my_handler);
 
@@ -85,20 +98,37 @@ fn main() {
         RocketHandlerOther::new(Method::Get, "/teststr".into(), handle_str, retrievers_str);
     let my_handler_str_route = Route::new(Method::Get, "/teststr", my_handler_str);
 
+    let retriever: UniqueStateRetriever<Counter> = UniqueStateRetriever::new();
+
     let my_handler_counter =
         RocketHandlerOther::new(Method::Get, "/count".into(), handle_count, retrievers_count);
     let my_handler_counter_route = Route::new(Method::Get, "/count", my_handler_counter);
 
-    let rocket = Rocket::ignite()
-        .manage(Counter::new())
-        .mount(
-            "/",
-            vec![
-                my_handler_route,
-                my_handler_str_route,
-                my_handler_a_route,
-                //my_handler_counter_route,
-            ],
-        )
-        .launch();
+    let my_handler_counter_deser = RocketHandlerOther::new(
+        Method::Post,
+        "/count_deser".into(),
+        handle_count_deser,
+        retrievers_count_deser,
+    );
+    let my_handler_counter_deser_route =
+        Route::new(Method::Post, "/count_deser", my_handler_counter_deser);
+    */
+    let endpoint_test = Endpoint::new_rocket("/test".into(), Method::GET, handle, retrievers);
+    let endpoint_deser_a =
+        Endpoint::new_rocket("/deser".into(), Method::POST, handle_a, retrievers_a);
+    let endpoint_counter =
+        Endpoint::new_rocket("/count".into(), Method::GET, handle_count, retrievers_count);
+    let endpoint_counter_deser = Endpoint::new_rocket(
+        "/count_deser".into(),
+        Method::POST,
+        handle_count_deser,
+        retrievers_count_deser,
+    );
+
+    let rocket = Rocket::ignite().manage(Arc::new(Counter::new()));
+    let mounter = RocketMounter::new(rocket);
+    mounter.mount_service(endpoint_test);
+    mounter.mount_service(endpoint_deser_a);
+    mounter.mount_service(endpoint_counter);
+    mounter.mount_service(endpoint_counter_deser);
 }
