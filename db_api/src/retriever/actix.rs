f33212e06badcb33e53529fc0f20683e458335f0
Olivier Pinon <op@impero.com>
2019-10-30 14:45:57 +0100

    Continue drafting actix_integ

--- db_api/src/retriever/actix.rs
+++ db_api/src/retriever/actix.rs
@@ -1,7 +1,12 @@
+use actix_web::web::Json;
 use actix_web::{FromRequest, HttpRequest};
 use futures::future::{Future, IntoFuture};
+use serde::de::DeserializeOwned;
 
-use crate::retriever::{Retriever, RetrieverBackend, StateRetriever};
+use crate::retriever::{
+    BodyRetriever, DeserializeRetriever, Retriever, RetrieverBackend, StateRetriever,
+    UniqueStateRetriever,
+};
 
 pub struct ActixRetriever<'a> {
     request: &'a HttpRequest,
@@ -30,6 +35,47 @@ where
         match T::extract(backend.request).into_future().wait() {
             Ok(t) => Ok(t),
             Err(_) => Err(ActixRetrieverError::Error),
+        }
+    }
+}
+
+impl<'a, T> Retriever<ActixRetriever<'a>, ActixRetrieverError> for BodyRetriever<T>
+where
+    T: FromRequest,
+{
+    type Output = T;
+
+    fn retrieve(&self, backend: &ActixRetriever<'a>) -> Result<Self::Output, ActixRetrieverError> {
+        match T::extract(backend.request).into_future().wait() {
+            Ok(t) => Ok(t),
+            Err(_) => Err(ActixRetrieverError::Error),
+        }
+    }
+}
+
+impl<'a, T> Retriever<ActixRetriever<'a>, ActixRetrieverError> for DeserializeRetriever<T>
+where
+    T: DeserializeOwned + 'static,
+{
+    type Output = T;
+
+    fn retrieve(&self, backend: &ActixRetriever<'a>) -> Result<Self::Output, ActixRetrieverError> {
+        match Json::<T>::extract(backend.request).into_future().wait() {
+            Ok(t) => Ok(t.into_inner()),
+            Err(_) => Err(ActixRetrieverError::Error),
+        }
+    }
+}
+
+impl<'a, T> Retriever<ActixRetriever<'a>, ActixRetrieverError> for UniqueStateRetriever<T>
+where
+    T: 'static,
+{
+    type Output = &'a T;
+    fn retrieve(&self, backend: &ActixRetriever<'a>) -> Result<Self::Output, ActixRetrieverError> {
+        match backend.request.app_data() {
+            Some(t) => Ok(t),
+            None => Err(ActixRetrieverError::Mismatch),
         }
     }
 }
